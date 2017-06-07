06641b3eeed9026e01fd8c89f64c7db3c05ac4a5
swethashri <tswethashri97@gmail.com>
2017-06-07 10:20:37 +0530

    Add files via upload

---
+++ swetha.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int number;
+printf("enter the number");
+scanf("%d",&number);
+if(number>0)
+printf("%d is a positive number \n",number);
+else
+printf("%d is negative number \n",number);
+return 0;}
