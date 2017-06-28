336c1354d33efdf04f9cad54537097c2f0c32a71
swethashri <tswethashri97@gmail.com>
2017-06-28 15:18:00 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,31 @@
+package logic;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+
+
+		      Scanner s = new Scanner(System.in); // use the Scanner class to read from stdin
+		    
+		       String s1=s.next();
+		       int n=s1.length();
+		       int k=s.nextInt(); 
+		      int a[]=new int[n];
+		       for(int i=0;i<n;i++)
+		           {
+		           a[i]=s1.charAt(i)-48;
+		       }
+		       Arrays.sort(a);
+		       for(int i=0;i<n-k;i++)
+		           System.out.print(a[i]);
+		   }
+		
+	}
+
+
