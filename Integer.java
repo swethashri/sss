afaf9ae39735ec7d80235fcb32bf8607b6d993b5
swethashri <tswethashri97@gmail.com>
2017-06-28 15:12:41 +0530

    Add files via upload

---
+++ Integer.java
@@ -0,0 +1,35 @@
+package logic;
+import java.io.*;
+import java.util.*;
+public class Integer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+
+
+
+
+						int x;
+						System.out.println("entered integer value");
+						
+				Scanner input =new Scanner (System.in);
+				 x = input.nextInt();
+				if(x%2==0)
+				 {
+					System.out.println("you entered even number");
+				 }
+				else
+				{
+					System.out.println("entered integer odd number");
+				}
+
+			}
+
+		
+
+	}
+
