1117afcb12d832df075dc16139b84c12dfce2df1
swethashri <tswethashri97@gmail.com>
2017-07-04 15:09:56 +0530

    Add files via upload

---
+++ Mul.java
@@ -0,0 +1,17 @@
+package guvi;
+import java.util.*;
+public class Mul {
+	public static void main(String[] args) {
+		int a,b,c;
+		System.out.println("Enter the first number");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		System.out.println("Enter the second number");
+		b=s.nextInt();
+		System.out.println("The multiplication is");
+		c=a*b;
+		System.out.println(c);
+
+	}
+
+}
