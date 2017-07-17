7d57c0fffd7a6a3363ab613e8eee8b33402298ea
swethashri <tswethashri97@gmail.com>
2017-07-17 09:54:12 +0530

    Add files via upload

--- Digits.java
+++ Digits.java
@@ -3,12 +3,11 @@ public class Digits {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int n=1234;
-int rev=1;
-while(n!=0)
+int num=1234;
+while(num!=0)
 {
-	int a=n%10;
-	n/=10;
+	int a=num%10;
+	num/=10;
 
 System.out.print(a);
 	}
