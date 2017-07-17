45b03d0e46777375e1319efcd9a5bed5d35c3cac
swethashri <tswethashri97@gmail.com>
2017-07-17 09:44:50 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int fact=1,num=5,i;
+for(i=1;i<=num;i++)
+		{
+	fact=fact*i;}
+	System.out.println(fact);
+		
+	}
+
+}
